8c67c774611e5b0ab1ee6f22c35b443e011b6ae2
Donald Yeh <Donald.Yeh@gmail.com>
2019-05-06 10:01:31 -0400

    adds skeleton of notes application

---
+++ src/NotesDetail.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotesDetail(prop) {
+	return <div>This is the notes detail</div>;
+}
---
+++ src/NotesList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NotesList(props) {
+	return (
+		<ul>
+			<li>this</li>
+			<li>is</li>
+			<li>the</li>
+			<li>notes</li>
+			<li>list</li>
+		</ul>
+	);
+}
