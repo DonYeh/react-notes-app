7e0ac4b5003e972bd840c0eb6b467652ff23997b
Donald Yeh <Donald.Yeh@gmail.com>
2019-05-06 14:21:29 -0400

    can now switch between notes and still toggle NoteDetail

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,15 +1,38 @@
 import React from "react";
 
-function NotesEditor({ text }) {
-	return <textarea value={text} />;
+function NotesEditor({ text, handleChange }) {
+	return (
+		<textarea
+			value={text}
+			onChange={e => {
+				handleChange(e.target.value);
+			}}
+		/>
+	);
 }
 
 export default class NotesDetail extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isEditing: false
+			isEditing: false,
+			draftText: props.note.text,
+			id: props.note.id
 		};
+	}
+
+	static getDerivedStateFromProps(props, state) {
+		// There is no `this`.
+		// So, we receive props and state as arguments.
+
+		// Must return an object that describes
+		// any modifications to state.
+		if (props.note.id !== state.id) {
+			return {
+				id: props.note.id,
+				draftText: props.note.text
+			};
+		}
 	}
 
 	render() {
@@ -17,14 +40,28 @@ export default class NotesDetail extends React.Component {
 		// an assigns them to the properties from this.props
 		// where the name matches
 		const { className, note } = this.props;
-		const { isEditing } = this.state;
+		const { isEditing, draftText } = this.state;
 		return (
 			<div className={className}>
-				{isEditing ? <NotesEditor text={note.text} /> : note.text}
+				{isEditing ? (
+					<NotesEditor
+						handleChange={this._changeDraftText}
+						text={draftText}
+					/>
+				) : (
+					draftText
+				)}
+				<br />
 				<button onClick={this._toggleIsEditing}>Toggle</button>
 			</div>
 		);
 	}
+
+	_changeDraftText = newText => {
+		this.setState({
+			draftText: newText
+		});
+	};
 
 	_toggleIsEditing = () => {
 		this.setState({
